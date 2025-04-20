8fec193ca9484214f55ffd72a19ce4ed2ae9d439
amithjayapraban <amithjayapraban@Amiths-MacBook-Air.local>
2025-04-21 00:44:00 +0530

    updates

--- index.js
+++ index.js
@@ -1,87 +1,155 @@
-const http = require("http");
-const websocket = require("websocket");
+import { createServer } from "http";
+import { server } from "websocket";
+import { createLogger, transports } from "winston";
+import { isIPv4, isIPv6 } from "net";
+import validator from "validator";
 
-const clients = {};
-let ip;
-const httpServer = http.createServer((req, res) => {
-  console.log(`${req.method.toUpperCase()} ${req.url}`);
+// Constants
+const UPDATE_INTERVAL_MS = 1000;
+const INACTIVITY_TIMEOUT_MS = 60000; // 60 seconds
 
+const logger = createLogger({
+  level: process.env.LOG_LEVEL || "info",
+  transports: [new transports.Console()],
+});
+
+const httpServer = createServer((req, res) => {
   const respond = (code, data, contentType = "text/plain") => {
+    const allowedOrigin = process.env.ALLOWED_ORIGIN || "*";
     res.writeHead(code, {
       "Content-Type": contentType,
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": allowedOrigin,
     });
     res.end(data);
   };
-
   respond(404, "Not Found");
 });
 
-const wsServer = new websocket.server({ httpServer });
+const clients = {};
+const pendingUpdates = new Set();
+
+setInterval(() => {
+  pendingUpdates.forEach((ip) => {
+    const keys = Object.keys(clients[ip] || {});
+    const message = JSON.stringify({ type: "peers", keys });
+
+    Object.values(clients[ip] || {}).forEach((client) => {
+      client.send(message);
+    });
+  });
+
+  pendingUpdates.clear();
+}, UPDATE_INTERVAL_MS);
+
+const wsServer = new server({ httpServer });
+
 wsServer.on("request", (req, socket) => {
-  console.log(`WS  ${JSON.stringify(req.resource)}`);
-  console.log(`Socket  ${JSON.stringify(req.remoteAddress)}`);
-  let ipparts = `${req.remoteAddress}`.split(":");
-  const first4Parts = ipparts.slice(0, 4);
-  ip = first4Parts.join(":");
-  console.log(first4Parts.join(":"));
-  if (ip == "::1" || ip == "::ffff:127.0.0.1") {
-    ip = "127.0.0.1";
+  let ip = req.remoteAddress;
+
+  if (isIPv6(ip) && ip.startsWith("::ffff:")) {
+    ip = ip.split("::ffff:")[1]; // Convert IPv6-mapped IPv4 to IPv4
   }
 
+  if (!isIPv4(ip)) {
+    logger.error("Invalid IP address");
+    req.reject();
+    return;
+  }
   const { path } = req.resourceURL;
   const splitted = path.split("/");
   splitted.shift();
   const id = splitted[0];
   const device = splitted[1];
-  const conn = req.accept(null, req.origin);
-  conn.on("message", (data) => {
-    if (data.type === "utf8") {
-      // console.log(`from ${id} << ${data.utf8Data}`);
 
-      const message = JSON.parse(data.utf8Data);
-      const destId = message.id;
-      const dest = clients[ip][destId];
-      if (dest) {
-        message.id = `${id}%${device}`;
-        const data = JSON.stringify(message);
-        console.log(`sending to ${destId} >> ${data}`);
-        dest.send(data);
-      } else {
-        console.error(`Client ${destId} not found`);
+  if (
+    !validator.isAlphanumeric(id) ||
+    !validator.isAlphanumeric(device) ||
+    id.length > 10 ||
+    device.length > 10
+  ) {
+    logger.error("Invalid client ID or device");
+    req.reject();
+    return;
+  }
+
+  const conn = req.accept(null, req.origin);
+
+  // To remove inactive clients
+  let inactivityTimeout = setTimeout(() => {
+    logger.warn(`Client ${id} at ${ip} timed out due to inactivity`);
+    conn.close(); // Close the connection if inactive
+  }, INACTIVITY_TIMEOUT_MS);
+
+  // Reset the timeout on any activity
+  const resetTimeout = () => {
+    clearTimeout(inactivityTimeout);
+    inactivityTimeout = setTimeout(() => {
+      logger.warn(`Client ${id} at ${ip} timed out due to inactivity`);
+      conn.close();
+    }, INACTIVITY_TIMEOUT_MS);
+  };
+
+  conn.on("message", (data) => {
+    resetTimeout(); // Reset timeout on receiving a message
+    if (data.type === "utf8") {
+      try {
+        const message = JSON.parse(data.utf8Data);
+        const destinationId = message.id;
+        const destination = clients[ip]?.[destinationId];
+
+        if (clients[ip] && destination) {
+          message.id = `${id}%${device}`;
+          const messageData = JSON.stringify(message);
+          logger.info(`sending to ${destinationId} >> ${messageData}`);
+          destination.send(messageData);
+        } else {
+          logger.error(`Client ${destinationId} not found`);
+        }
+      } catch (error) {
+        logger.error(
+          `Invalid message format from client ${id} at ${ip}:`,
+          error
+        );
+        return;
       }
     }
   });
-  conn.on("close", () => {
-    delete clients[`${ip}`][`${id}%${device}`];
 
-    if (Object.keys(clients[`${ip}`]).length == 0) {
-      delete clients[`${ip}`];
-      console.log("rooom deleted");
+  conn.on("close", () => {
+    delete clients[ip]?.[`${id}%${device}`];
+
+    if (clients[ip] && Object.keys(clients[ip]).length == 0) {
+      delete clients[ip];
+      pendingUpdates.delete(ip);
+      logger.info("room deleted");
     } else {
-      let keys = Object.keys(clients[ip]);
-      Object.values(clients[`${ip}`]).forEach((i) => {
-        i.send(JSON.stringify({ type: "peers", keys }));
-      });
+      pendingUpdates.add(ip);
     }
-    console.error(`Client ${id} disconnected`);
+    logger.warn(`Client ${id} disconnected`);
   });
-  if (!clients[`${ip}`]) {
-    clients[`${ip}`] = {};
+  if (!clients[ip]) {
+    clients[ip] = {};
+    logger.info(`New room created for IP ${ip}`);
   }
 
-  clients[`${ip}`][`${id}%${device}`] = conn;
-  let keys = Object.keys(clients[`${ip}`]);
-  Object.values(clients[`${ip}`]).forEach((i) => {
-    i.send(JSON.stringify({ type: "peers", keys }));
-  });
-  console.log(Object.keys(clients[ip]), "clients");
+  if (clients[ip][`${id}%${device}`]) {
+    logger.warn(
+      `Client ${id} (${device}) at ${ip} is reconnecting, replacing the old connection`
+    );
+  }
+
+  clients[ip][`${id}%${device}`] = conn;
+  pendingUpdates.add(ip);
+  logger.info(`Client ${id} (${device}) at ${ip} added to clients object`);
+  logger.debug(
+    `Current clients for IP ${ip}: ${Object.keys(clients[ip]).join(", ")}`
+  );
 });
 
-const endpoint = process.env.PORT || "8080";
-const splitted = endpoint.split(":");
-const port = splitted.pop();
+const port = parseInt(process.env.PORT, 10) || 8080;
 
 httpServer.listen(port, () => {
-  console.log(`Server listening on ${port}`);
+  logger.info(`Server listening on port ${port}`);
 });
+
+
